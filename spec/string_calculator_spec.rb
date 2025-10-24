287b2c01590f4a913307e5cb01a94241c620e617
yashk03 <yash.khadse@smilevirtualconsult.com>
2025-10-24 23:03:07 +0530

    Test 3: support multiple numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,5 +13,9 @@ RSpec.describe StringCalculator do
     it "returns the sum for two comma-separated numbers" do
       expect(StringCalculator.add("1,5")).to eq(6)
     end
+
+    it "returns the sum for an unknown quantity of numbers" do
+      expect(StringCalculator.add("1,2,3,4,5")).to eq(15)
+    end
   end
 end
